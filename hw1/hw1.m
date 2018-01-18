5d1963d20e16008650bf93f71bc7f9d0e4d5a784
mckib2 <nicholas.bgp@gmail.com>
2018-01-18 14:25:36 -0700

    Initial commit

---
+++ hw1/hw1.m
@@ -0,0 +1,158 @@
+%% Homework 1
+% Nicholas McKibben
+% MEEn 575
+% 2018-01-11
+
+clear;
+close all;
+
+%% Get some extrema points
+N = 5;
+[ x0_neg_sqp,xopt0_neg_sqp,fopt0_neg_sqp,~,history ] = optimize_spring(N,@(x) -x,'sqp');
+
+%% Contour Plots
+% We'll have to keep a couple variables contant to represent the space on
+% the contour plot.  We'll choose d vs D and hold n,hf constant.
+
+w = .18;
+Q = 15e4;
+G = 12e6;
+Se = 45e3;
+Sf = 1.5;
+h0 = 1;
+delta0 = .4;
+hdef = h0 - delta0;
+hf = xopt0_neg_sqp(1,4);
+n = xopt0_neg_sqp(1,3);
+d0 = xopt0_neg_sqp(1,1);
+D0 = xopt0_neg_sqp(1,2);
+F0 = -fopt0_neg_sqp(1);
+
+d = linspace(.02,.075,300);
+D = linspace(.3,.8,300);
+[X,Y] = meshgrid(d,D);
+
+Sy = 0.44*Q./(d0^w);
+hs = n*X;
+k = G*X.^4./(8*Y.^3*n);
+K = (4*Y - X)./(4*(Y - X)) + 0.62*X./Y;
+m = 8*K.*Y./(pi*X.^3);
+t_hs = (hf - hs).*k.*m;
+tmax = (hf - h0 + delta0).*k.*m;
+tmin = (hf - h0).*k.*m;
+tm = (tmax + tmin)./2;
+ta = (tmax - tmin)./2;
+
+figure(1);
+
+% Change size of figure
+xywh = get(gcf,'position');
+units = get(gcf,'units');
+set(gcf,'units',units,'position',[xywh(1),xywh(2),800,500]);
+
+ls = 1000; % label spacing
+[ C,h ] = contour(X,Y,(hf - h0)*k,'k'); % plot the force
+clabel(C,h,'LabelSpacing',ls);
+
+% Plot the constraint contours
+hold on;
+[ C,h ] = contour(X,Y,Y./X,[4,16],'r','LineWidth',2); % 4 <= D/d <= 16
+clabel(C,h,'LabelSpacing',ls);
+
+[ C,h] = contour(X,Y,X + Y,[.75,.75],'g','LineWidth',2); % D + d < 0.75 % active
+clabel(C,h,'LabelSpacing',ls);
+
+[ C,h ] = contour(X,Y,hdef - hs,[.05,.05],'b','LineWidth',2); % hdef - hs > 0.05 % active
+clabel(C,h,'LabelSpacing',ls);
+
+[ C,h ] = contour(X,Y,ta,[Se/Sf,Se/Sf],'y','LineWidth',2); % ta <= Se/Sf
+clabel(C,h,'LabelSpacing',ls);
+
+[ C,h ] = contour(X,Y,ta + tm,[Sy/Sf,Sy/Sf],'cy','LineWidth',2); % ta + tm <= Sy/Sf
+clabel(C,h,'LabelSpacing',ls);
+
+[ C,h ] = contour(X,Y,t_hs,[Sy,Sy],'m','LineWidth',2); % t_hs < Sy % active
+clabel(C,h,'LabelSpacing',ls);
+
+% Plot the point
+plot(d0,D0,'r*','MarkerSize',10,'LineWidth',2);
+text(d0-.0025,D0+.062,sprintf('d = %f\nD = %f\nF = %f',d0,D0,F0),'HorizontalAlignment','center','BackgroundColor',[.6,.6,.6]);
+
+% Shade the feasible region
+xfeas = [ .0239;.04426;.072437;.0496 ];
+yfeas = [ .3853;.7082;.677563;.5542 ];
+h = fill(xfeas,yfeas,[.9,.9,.9],'LineStyle','none');
+uistack(h,'bottom');
+
+title('Feasible Space');
+xlabel('wire diameter, d (in)');
+ylabel('coil diameter, D (in)');
+legend('Feasible Region','F','4 <= D/d <= 16','D + d < 0.75','h_{def} - h_s > .05','t_a <= S_e/S_f|_{d=d_{opt}}','t_a + t_m <= S_y/S_f|_{d=d_{opt}}','t_hs < Sy','Location','northwest','Optimum');
+
+%% Plot the histories
+% close all;
+
+for nn = 1:size(history,2)
+    figure(nn + 1);
+    h = history(nn);
+    xmin = min(h.x(:,1));
+    xmax = max(h.x(:,1));
+    ymin = min(h.x(:,2));
+    ymax = max(h.x(:,2));
+    
+    [X,Y] = meshgrid(linspace(xmin,xmax,300),linspace(ymin,ymax,300));
+    k = G*X.^4./(8*Y.^3.*n);
+    [ C,hh ] = contour(X,Y,(hf - h0)*k,'k'); % plot the force
+    clabel(C,hh,'LabelSpacing',ls);
+    title(sprintf('Start Point %d',nn));
+    xlabel('wire diameter, d (in)');
+    ylabel('coil diameter, D (in)');
+    hold on;
+
+    plot(h.x(:,1),h.x(:,2),'-r.');
+    for xx = 1:size(h.x)
+        plot(h.x(xx,1),h.x(xx,2));
+        text(h.x(xx,1),h.x(xx,2),num2str(xx),'FontSize',7);
+    end
+    
+    h = fill(xfeas,yfeas,[.9,.9,.9],'LineStyle','none');
+    uistack(h,'bottom');
+end
+
+%% Generate some tables
+
+% Show several starting points
+startingpoints = table( ...
+    x0_neg_sqp(:,1), ...
+    x0_neg_sqp(:,2), ...
+    x0_neg_sqp(:,3), ...
+    x0_neg_sqp(:,4), ...
+    'VariableNames',...
+    {'d0','D0','n0','hf0'}, ...
+    'RowNames', ...
+    {'x_init0','x_init1','x_init2','x_init3','x_init4'});
+disp(startingpoints);
+
+% Show several end points and function values to verify we get to the same
+% optimum point
+endpoints = table( ...
+    xopt0_neg_sqp(:,1), ...
+    xopt0_neg_sqp(:,2), ...
+    xopt0_neg_sqp(:,3), ...
+    xopt0_neg_sqp(:,4), ...
+    -fopt0_neg_sqp, ...
+    'VariableNames', ...
+    {'d_opt','D_opt','n_opt','hf_opt','F'}, ...
+    'RowNames', ...
+    {'x_opt0','x_opt1','x_opt2','x_opt3','x_opt4'});
+disp(endpoints);
+    
+% Optimum Values
+dvars = table( ...
+    xopt0_neg_sqp(1,:)', ...
+    'VariableNames', ...
+    {'DesignVariables'}, ...
+    'RowNames', ...
+    {'wire diameter, d','coil diameter, D','coil number, n','free height, hf'});
+disp(dvars);
+
---
+++ hw1/optimize_spring.m
@@ -0,0 +1,117 @@
+function [x0,xopt,fopt,exitflag,histories] = optimize_spring(N,cfun,algorithm)
+
+    % ------------Starting point and bounds------------
+    %x0 = [ .05,.5,10,1.5 ]; % starting point
+    %      d    D    n     hf
+    ub = [ .2,  3.2, +Inf, +Inf ]; % upper bound
+    lb = [ .01, .04, 1,    1 ]; % lower bound
+
+    % ------------Linear constraints------------
+    A = [];
+    b = [];
+    Aeq = [];
+    beq = [];
+
+    % ------------Objective and Constraints------------
+    function [f, c, ceq] = objcon(x)
+        
+        % design variables
+        d = x(1);  % wire diameter (in)
+        D = x(2);  % coil diameter (in)
+        n = x(3);  % number of coils
+        hf = x(4); % free height (in)
+        
+        % other analysis variables
+        delta0 = 0.4;
+        h0 = 1.0;
+        Q = 15e4;
+        G = 12e6;
+        Se = 45e3;
+        Sf = 1.5;
+        w = 0.18;
+        
+        % analysis functions
+        Sy = 0.44*Q/d^w;
+        hs = n*d;
+        k = G*d^4/(8*D^3*n);
+        K = (4*D - d)/(4*(D - d)) + 0.62*d/D;
+        m = 8*K*D/(pi*d^3);
+        tmax = (hf - h0 + delta0)*k*m;
+        tmin = (hf - h0)*k*m;
+        tm = (tmax + tmin)/2;
+        ta = (tmax - tmin)/2;
+        hdef = h0 - delta0;
+        t_hs = (hf - hs)*k*m; % stress at solid height
+        F = k*(hf - h0);
+
+        % objective function
+        %f = 1/F; % maximize force by minimizing 1/F
+        f = cfun(F);
+        
+        %inequality constraints (c<=0)
+        c = zeros(7,1);         % create column vector
+        c(1) = 4 - D/d;         % 4 <= D/d
+        c(2) = D/d - 16;        % D/d <= 16
+        c(3) = .05 - hdef + hs; % hdef - hs > 0.05
+        c(4) = ta - Se/Sf;      % ta <= Se/Sf
+        c(5) = ta + tm - Sy/Sf; % ta + tm <= Sy/Sf
+        c(6) = t_hs - Sy;    % t_hs < Sy
+        c(7) = D + d - .75;     % D + d < .75
+        
+        %equality constraints (ceq=0)
+        ceq = [];
+
+    end
+
+
+    % ------------Call fmincon N times------------
+    x0 = zeros(N,4);
+    xopt = x0;
+    fopt = zeros(N,1);
+    for nn = 1:N
+        % Find a feasible starting point
+        [x00,~,~,~] = linprog(ones(1,4)/2,A,b,Aeq,beq,lb,ub);
+        x00 = x00.' + nn.*rand(1,4);
+        x0(nn,:) = x00;
+        
+        history.x = [];
+        history.fval = [];
+        options = optimoptions(@fmincon,'OutputFcn',@outfun,'MaxFunctionEvaluations',Inf,'MaxIterations',Inf,'Algorithm',algorithm);
+        [xopt(nn,:),fopt(nn,:),exitflag,~] = fmincon(@obj,x00,A,b,Aeq,beq,lb,ub,@con,options);
+        histories(nn) = history;
+    end
+        
+    % ------------Separate obj/con (do not change)------------
+    function stop = outfun(x,optimValues,state)
+        stop = false;
+        switch state
+            case 'init'
+                hold on;
+            case 'iter'
+            % Concatenate current point and objective function
+            % value with history. x must be a row vector.
+              history.fval = [history.fval; optimValues.fval];
+              history.x = [history.x; x];
+            % Concatenate current search direction with 
+            % searchdir.
+              %searchdir = [searchdir;... 
+              %             optimValues.searchdirection'];
+              plot(x(1),x(2),'o');
+            % Label points with iteration number and add title.
+            % Add .15 to x(1) to separate label from plotted 'o'
+              text(x(1)+.15,x(2),... 
+                   num2str(optimValues.iteration));
+              title('Sequence of Points Computed by fmincon');
+            case 'done'
+                hold off;
+            otherwise
+         end
+     end
+    
+    function [f] = obj(x) 
+        [f, ~, ~] = objcon(x);
+    end
+    function [c, ceq] = con(x) 
+        [~, c, ceq] = objcon(x);
+    end
+end
