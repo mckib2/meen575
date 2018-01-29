2451ae1787c207152f573ac5615ca9399eb680c8
mckib2 <nicholas.bgp@gmail.com>
2018-01-28 21:23:40 -0700

    Started work on design project

--- design-project/proj.py
+++ design-project/proj.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.spatial import Voronoi
+from scipy.spatial import Voronoi, voronoi_plot_2d
+
+def PolyArea(x,y):
+    return(0.5*np.abs(np.dot(x,np.roll(y,1)) - np.dot(y,np.roll(x,1))))
 
 # Kaiser-Bessel window
 M = 10
@@ -11,22 +14,34 @@ win = np.kaiser(M,beta)
 a = 5
 b = 7
 delta = np.pi/2
-t = np.linspace(-np.pi,np.pi,300)
+N = 75
+t = np.linspace(-np.pi,np.pi,N)
 x = np.sin(a*t + delta)
 y = np.sin(b*t)
 
 # Let's look at it just for fun
 plt.figure()
-plt.plot(x,y)
+plt.scatter(x,y)
+plt.plot(x,y,linewidth=0.5)
 plt.show()
 
-# We also need a way to sample it
-xs = []
-ys = []
-Sj = zip(xs,ys)
+Sj = zip(x,y)
 
 # We also need a voronoi diagram!
 opts = []
 vor = Voronoi(Sj,opts)
+voronoi_plot_2d(vor)
+plt.show()
+
+#idx = vor.regions
+#verts = []
+#for reg_idx,reg in enumerate(idx):
+#    verts.append([])
+#    for idx_idx,val in enumerate(reg):
+#        verts[reg_idx].append(vor.vertices[val])
+#    print((verts[reg_idx],verts[reg_idx]))
+#    areas = PolyArea(verts[reg_idx,0],verts[reg_idx,1])
+        
+#print(len(areas))
 
 # We'll also need to load in the Shepp-Loagan phantom
